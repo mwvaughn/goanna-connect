a46c2ee9e36099d06e22e67516d36e8baf5ce770
Matthew Vaughn <vaughn@tacc.utexas.edu>
2012-12-19 08:50:06 -0700

    Final fixes

--- goannashim.pl
+++ goannashim.pl
@@ -14,6 +14,8 @@ my $file_type;
 my $search_field;
 my $ID_LIST;
 my $DATABASE;
+my $DATABASE2;
+my $DATABASE3;
 my $no_iea;
 my $EXPECT;
 my $WORD_SIZE;
@@ -24,6 +26,7 @@ my $ALIGNMENTS;
 my $bypass_prg_check;
 my $error;
 my $submit;
+
 
 ##Below are all of the evidence codes which GOanna supports. They must be passed explicitly.
 
@@ -50,7 +53,7 @@ my $NR;
 my $RCA;
 my $TAS;
 
-GetOptions("PROGRAM=s" => \$PROGRAM, 'EMAIL=s' => \$EMAIL, 'file_type=s' => \$file_type, 'search_field=s' => \$search_field, 'ID_LIST=s' => \$ID_LIST, 'DATABASE=s{1,3}' => \$DATABASE, 'no_iea=i' => \$no_iea, 'EXPECT=s' => \$EXPECT, 'WORD_SIZE=s' => \$WORD_SIZE,
+GetOptions("PROGRAM=s" => \$PROGRAM, 'EMAIL=s' => \$EMAIL, 'file_type=s' => \$file_type, 'search_field=s' => \$search_field, 'ID_LIST=s' => \$ID_LIST, 'DATABASE=s' => \$DATABASE, 'DATABASE2=s' => \$DATABASE2, 'DATABASE3=s' => \$DATABASE3, 'no_iea=s' => \$no_iea, 'EXPECT=s' => \$EXPECT, 'WORD_SIZE=s' => \$WORD_SIZE,
  'GAPCOSTS=s' => \$GAPCOSTS, 'DESCRIPTIONS=i' => \$DESCRIPTIONS, 'ALIGNMENTS=i' => \$ALIGNMENTS,
   'EXP=s' => \$EXP, 'IBA=s' => \$IBA, 'IBD=s'  => \$IBD, 'IC=s' => \$IC, 'IDA=s' => \$IDA, 'IEA=s' => \$IEA, 'IEP=s' => \$IEP, 'IGC=s' => \$IGC, 'IGI=s' => \$IGI, 'IKR=s' => \$IKR, 'IMP=s' => \$IMP, 'IPI=s' => \$IPI, 'IRD=s' => \$IRD, 'ISA=s' => \$ISA, 'ISM=s' => \$ISM, 'ISO=s' => \$ISO, 'ISS=s' => \$ISS, 'NAS=s' => \$NAS, 'ND=s' => \$ND, 'NR=s' => \$NR, 'RCA=s' => \$RCA, 'TAS=s' => \$TAS,
     'bypass_prg_check=i' => \$bypass_prg_check, 'error=i' => \$error);
@@ -75,6 +78,8 @@ my $req = (POST 'http://agbase.hpc.msstate.edu/cgi-bin/tools/GOanna.cgi',
 						'EMAIL' => $EMAIL,
 						'file_type' =>  $file_type,
 						'DATABASE' => $DATABASE,
+						'DATABASE' => $DATABASE2,
+						'DATABASE' => $DATABASE3,
 						'MATRIX_NAME' => 'BLOSUM62',
 						'search_field' => 'Select ID',
 						'EXPECT' => '10e-1',
@@ -125,18 +130,38 @@ my $job_id;
 	my $out_filename = join( "", $job_id, ".zip");
 	#-- fetch the zip and save it as perlhowto.zip
 	my $status = getstore($out_url, $out_filename);
-until ( is_success($status) ){
-	print "Error downloading file: $status\n";
-	print "Trying again in 15 seconds\n";
-	sleep(15);
-	$status = getstore($out_url, $out_filename);
+my $maxwait = 1200;
+my $initwait = 10;
+my $totalwait = 0;
+if (defined $job_id){
+
+	until ( is_success($status) ){
+		print "Error downloading file: $status\n";
+		print "Trying again in $initwait seconds\n";
+		sleep($initwait);
+		$initwait = $initwait*1.2;
+		if ($initwait > $maxwait){
+			$initwait=$maxwait;
+
+}
+		$totalwait=$totalwait+$initwait;
+		$status = getstore($out_url, $out_filename);
+if ($totalwait > 36000) {
+print "Maximum wait time exceeded; either GOanna is down or the submission will never finish.";
+exit;
+}
 }
 if ( is_success($status) ) {
 			print "The GOanna file downloaded correctly\nIts name is $out_filename\n";
 			sleep(3);
 			exit;
 			}
+}
 
+else {
+print "The submission failed, likely because of bad parameters\n";
+exit;
+}
 
 
 
